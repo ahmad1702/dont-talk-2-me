5b69e67c78bf29c0a51957369bbdc42d029f367f
Ahmad Sandid <ahmad1702@gmail.com>
2022-08-31 22:02:25 -0500

    env setup

--- pages/api/messages.ts
+++ pages/api/messages.ts
@@ -10,20 +10,28 @@ export default function handler(
     const body = req.body;
     if (body) {
       if (body.username && body.message && body.time) {
-        try {
-          const pusher = new Pusher({
-            appId: "1471406",
-            key: "49d47e2ad5cd2baff292",
-            secret: "377e68a33529233e4b38",
-            cluster: "us2",
-            useTLS: true,
-          });
-          console.log("API CALL TO PUSHER");
-          const { room } = req.query;
-          pusher.trigger(room ? room : "my-channel", "my-event", body);
-          res.status(200).json({ success: "success" });
-        } catch (error) {
-          res.status(500).json({ error: error });
+        const appId: string | undefined = process.env.NEXT_PUBLIC_PUSHER_APP_ID;
+        const key: string | undefined = process.env.NEXT_PUBLIC_PUSHER_APP_KEY;
+        const secret: string | undefined =
+          process.env.NEXT_PUBLIC_PUSHER_SECRET;
+        if (appId && key && secret) {
+          try {
+            const pusher = new Pusher({
+              appId: appId,
+              key: key,
+              secret: secret,
+              cluster: "us2",
+              useTLS: true,
+            });
+            console.log("API CALL TO PUSHER");
+            const { room } = req.query;
+            pusher.trigger(room ? room : "my-channel", "my-event", body);
+            res.status(200).json({ success: "success" });
+          } catch (error) {
+            res.status(500).json({ error: error });
+          }
+        } else {
+          res.status(500).json({ message: "Env not working" });
         }
       } else {
         res.status(400).json({ message: "Incorrect body supplied" });
