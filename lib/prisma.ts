6d2d29e4fd513720dfdfbb702e35376d6bf2d611
Ahmad Sandid <ahmad1702@gmail.com>
2022-09-01 00:15:58 -0500

    gitignore fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,25 @@
-// declare global {
-//   var prisma: PrismaClient; // This must be a `var` and not a `let / const`
-// }
+declare global {
+  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
+}
+
+// import { PrismaClient } from "@prisma/client";
+// let prisma: PrismaClient;
+
+// // if (process.env.NODE_ENV === "production") {
+// prisma = new PrismaClient();
+// // } else {
+// //   if (!global.prisma) {
+// //     global.prisma = new PrismaClient();
+// //   }
+// //   prisma = global.prisma;
+// // }
+
+// export default prisma;
 
 import { PrismaClient } from "@prisma/client";
-let prisma: PrismaClient;
 
-// if (process.env.NODE_ENV === "production") {
-prisma = new PrismaClient();
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
+const prisma = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
